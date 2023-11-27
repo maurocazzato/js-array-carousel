163de8c12e553692f15f89c25792de4c5d1a21fa
Maurocazzato <maurizzio.125@gmail.com>
2023-11-27 10:05:06 +0100

    consegna js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Creare un carosello come nello screenshot allegato.
+// MILESTONE 1
+// Per prima cosa, creiamo il markup statico: l’html con tutto il necessario perchè funzioni il carosello.
+// (Se su qualche parte del layout vedete che ci state perdendo troppo tempo tralasciate i dettagli e passate alla parte logica il prima possibile).
+// MILESTONE 2
+// Al click dell’utente sulle frecce (sia Prev che Next), il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
